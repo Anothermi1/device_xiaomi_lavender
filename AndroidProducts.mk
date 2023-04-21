82518eebf2b105a9fee35d9bc47966336ec491d3
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-04-21 10:10:30 +0600

    lavender: trigger NusantaraOs

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,7 @@
-#
-# Copyright (C) 2019 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_lavender.mk
+    $(LOCAL_DIR)/nad_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    nad_lavender-user \
+    nad_lavender-userdebug \
+    nad_lavender-eng
