980865124ffe929b23706a7df6fa291d4fa2ce30
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-04-27 20:54:59 +0600

    lavender: import device.mk

--- device.mk
+++ device.mk
@@ -347,6 +347,12 @@ PRODUCT_COPY_FILES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson
+    
+PRODUCT_PACKAGES += \
+   android.frameworks.sensorservice@1.0.vendor \
+   android.hardware.camera.provider@2.4-impl \
+   android.hardware.camera.provider@2.4-service_64 \
+   vendor.qti.hardware.camera.device@1.0.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -474,4 +480,3 @@ PRODUCT_PACKAGES += \
     
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
-
