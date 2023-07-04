7956f8612f23cb571513e5ead4a4deea0c7eb30a
Yusna21 <yusromi04@gmail.com>
2023-07-04 07:11:38 +0700

    lavender: Ready for ProtonPlus

--- lavender.mk
+++ lavender.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/proton/config/common.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -20,11 +20,9 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := lavender
 PRODUCT_MODEL := Redmi Note 7/7S
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-LESSAOSP_BUILD_TYPE := UNOFFICIAL
-LESSAOSP_MAINTAINER := Yusna21
-WITH_GMS := true
+WITH_GAPPS := true
 TARGET_VENDOR_PRODUCT_NAME := lavender
