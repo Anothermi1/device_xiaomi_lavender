ef47c84f3219337b2c0710873f5a9d9addcfa35d
Yusna21 <yusromi04@gmail.com>
2023-07-07 05:45:35 +0000

    lavender: BringUp YAAP

--- yaap_lavender.mk
+++ yaap_lavender.mk
@@ -9,13 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Official-ify
 # VOLTAGE_BUILD_TYPE := OFFICIAL
 
 # Boot animation
-TARGET_BOOT_ANIMATION_RES := 2160
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -23,7 +24,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := voltage_lavender
+PRODUCT_NAME := yaap_lavender
 PRODUCT_MODEL := Redmi Note 7/7S
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
